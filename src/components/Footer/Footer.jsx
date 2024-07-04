ded42ecd2e3223a7fb09a32b4dd2d65ea435effb
Jiho Sohn <jsohndata@gmail.com>
2024-07-04 18:18:58 -0400

    UPDATE mozart ghost id

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
         </Row>
       </Container>
 
-      <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=11"
+      <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=sytha"
         alt="Click on it and press control-shift."
         title="Click on it and press control-shift.">π</a></p>
     </footer>
